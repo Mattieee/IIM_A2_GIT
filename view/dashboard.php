f89ccd637b676f6a55963c0abb20f9643d0f56a4
romzych <rblondel23@gmail.com>
2018-04-18 14:52:06 +0200

    created(like button)

--- view/dashboard.php
+++ view/dashboard.php
@@ -36,6 +36,8 @@
 										</ul>
 									</div>
 									<b class="username">Posté par <?php echo $music['username']; ?></b>
+                                    <button id="counter" onclick="count(); toggleText(this.id);">Like</button>
+                                    <span id="keeper"> 0 </span>
 									<h3 class="title">
 										<?php echo $music['title']; ?>
 									</h3>
